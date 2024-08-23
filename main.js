8dcb0ee9ecc6cba4d603b11360183ad5a4eeb99b
beejayofgod <adexbolaji100@gmail.com>
2024-08-23 19:24:38 +0100

    slider

---
+++ main.js
@@ -0,0 +1,60 @@
+let slider_items = document.querySelectorAll(".slider-item");
+let nextBtns = document.querySelectorAll(".arrow-right");
+let prevBtns = document.querySelectorAll(".arrow-left");
+let dashes = document.querySelectorAll(".dash");
+
+let slide_index = 0;
+
+function showSlide(nextindex) {
+  if (nextindex == 3) slide_index = 0; // restart slide
+  else if (nextindex < 0) slide_index = slider_items.length - 1; // restart slider
+
+  slider_items.forEach((item) => (item.style.display = "none"));
+  dashes.forEach((dash) => (dash.style.opacity = "0.5"));
+
+  dashes[slide_index].style.opacity = "1";
+  slider_items[slide_index].style.display = "flex";
+}
+
+function autoplay() {
+  slide_index += 1;
+  showSlide(slide_index);
+}
+
+showSlide(slide_index); // show first slider
+
+let autoplayInterval = setInterval(autoplay, 15000);
+
+nextBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    slide_index += 1;
+    showSlide(slide_index);
+
+    clearInterval(autoplayInterval);
+    autoplayInterval = setInterval(autoplay, 10000);
+  });
+});
+
+prevBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    slide_index -= 1;
+    showSlide(slide_index);
+
+    clearInterval(autoplayInterval);
+    autoplayInterval = setInterval(autoplay, 10000);
+  });
+});
+
+let dashArray = Array.from(dashes);
+
+dashArray.forEach((dash) => {
+  dash.addEventListener("click", () => {
+    let dashIndex = dashArray.indexOf(dash);
+
+    slider_items.forEach((item) => (item.style.display = "none"));
+    dashes.forEach((dash) => (dash.style.opacity = "0.5"));
+
+    dashes[dashIndex].style.opacity = "1";
+    slider_items[dashIndex].style.display = "flex";
+  });
+});
